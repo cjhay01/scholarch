0983dd234dd8498797aaff7b93ef43a5a64663cd
cjhay <71071772+cjhay01@users.noreply.github.com>
2026-05-21 12:02:52 +0800

    Revert "fix"
    This reverts commit 005eb8da1da91c85e5e8279d7fb9eccfadc5be22.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,7 @@ dotenv.config({ quiet: true });
 const { setServers } = require("node:dns/promises");
 setServers(["1.1.1.1", "8.8.8.8"]);
 
+connectDB();
 
 const app = express();
 
@@ -40,7 +41,6 @@ app.use((err, req, res, next) => {
     });
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-connectDB();
